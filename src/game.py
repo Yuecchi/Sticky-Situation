b2a84930262033e2e3e630459e0002d6c15c361e
tefloncat <elliotcarter@talktalk.net>
2019-03-04 15:16:31 +0000

    all tile classes are now functional and it is possible to draw tilemaps

--- src/game.py
+++ src/game.py
@@ -4,11 +4,24 @@ except ImportError:
     import SimpleGUICS2Pygame.simpleguics2pygame as simplegui
 
 from tileEngine import Tilesheet
+from tileEngine import Tilemap
 
 # testing tilesheets
 img = simplegui.load_image('https://i.imgur.com/1v3BBoO.png')
 index = (1, 1, 10, 1)
+
 tilesheet = Tilesheet(img, index)
+tilesheet.tiles[2].set_animation_speed(6)
+
+map = [[3, 3, 3, 3, 3],
+       [3, 0, 1, 0, 3],
+       [3, 1, 2, 1, 3],
+       [3, 0, 1, 0, 3],
+       [3, 3, 3, 3, 3]]
+
+tilemap = Tilemap(tilesheet, map)
+
+
 
 
 
--- src/gfx.py
+++ src/gfx.py
@@ -1,19 +1,34 @@
 import game
-import math
 
 TILESIZE = 32
 HALFSIZE = 16
 
+class Clock:
+
+    def __init__(self):
+        self.t = 0
+
+    def tick(self):
+        self.t += 1
+
+    def transition(self, rate):
+        return not(self.t % rate)
+
+clock = Clock()
+
+
 def draw(canvas):
+
+    global clock
+
     game.tilesheet.draw(canvas)
+    game.tilemap.draw(canvas)
 
-    for i in range(game.tilesheet.tilecount):
-        game.tilesheet.tiles[i].draw(canvas, HALFSIZE + (TILESIZE * i), HALFSIZE)
+    #for i in range(game.tilesheet.tilecount):
+    #    game.tilesheet.tiles[i].draw(canvas, HALFSIZE + (TILESIZE * i), HALFSIZE)
+    #canvas.draw_text(str(game.tilesheet.tiles[2].current_index), (0, 64), 16, "White")
 
-    canvas.draw_text(str(game.tilesheet.tiles[2].current_index), (0, 64), 16, "White")
-
-    for i in range(1000000):
-        math.sqrt(2)
+    clock.tick()
 
 
 
